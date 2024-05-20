99b97ebc81028612c127f886308cfc72ddddfd59
parasrawat007 <parasrawat67@gmail.com>
2024-05-20 19:36:12 +0530

    Duplicates in Sorted Array

--- 55_FindingDuplicatesForSortedArray.cpp
+++ 55_FindingDuplicatesForSortedArray.cpp
@@ -1,35 +1,13 @@
 #include<iostream>
 using namespace std;
 
-int max(int a[],int n)
-{
-    int max=a[0];
-    for (int i = 1; i < n; i++)
-    {
-        if(a[i]>max)
-            max=a[i];
-    }
-    return max;    
-}
-
-int min(int a[],int n)
-{
-    int min=a[0];
-    for (int i = 1; i < n; i++)
-    {
-        if(a[i]<min)
-            min=a[i];
-    }
-    return min;    
-}
-
 int main()
 {
     int A[10]={3,6,8,8,10,12,15,15,15,20};
     
     int n=10;
-    int l=min(A,n);
-    int h=max(A,n);
+    int l=A[0];
+    int h=A[9];
 
     int *H= new int[h];
 
