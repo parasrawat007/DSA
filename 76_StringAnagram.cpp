55782b6ed5356085fbf926132d5a0e68224aa301
parasrawat007 <parasrawat67@gmail.com>
2024-05-28 21:54:00 +0530

    Anagram Strings

---
+++ 76_StringAnagram.cpp
@@ -0,0 +1,29 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    char A[]="decimal";
+    char B[]="medical";
+    int H[26]={0};
+    int i;
+
+    for (i = 0; A[i]!='\0'; i++)
+    {
+        H[A[i]-97]++;
+    }
+    
+    for ( i = 0; B[i]!='\0'; i++)
+    {
+        H[B[i]-97]--;
+        if(H[i]!=0)
+        {
+            cout<<"Not Anagram";
+            break;
+        }
+    }
+    if(B[i]=='\0')
+        cout<<"Anagram";
+
+    return 0;
+}
